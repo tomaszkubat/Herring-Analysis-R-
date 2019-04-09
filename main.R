a6dec957ebf2a2c595434380ea77649d414a4551
Tomasz Kubat <tomasz.kubat@wp.pl>
2019-04-09 22:04:54 +0200

    TK clean data script added (draft)

---
+++ main.R
@@ -0,0 +1,16 @@
+## TK
+## 2019/04/09
+
+
+# start operations
+globalVariables = list() # create empty list for global variables
+globalVariables[['wd']] <- dirname(rstudioapi::getSourceEditorContext()$path) # get working directory
+setwd(globalVariables$wd) # set working directory
+
+globalVariables[['requiredPackages']] <- c('dplyr') # required packages
+
+
+# load data from files
+source('scripts/tools/functions.R') # load user-defined functions
+
+loadInstall(globalVariables$requiredPackages) # load/install requred packages
--- scripts/data/cleanData.R
+++ scripts/data/cleanData.R
@@ -1,12 +1,21 @@
-# load list of packages (and install if necessary) 
-loadInstall <- function(x){
-  for(i in x) { # x - list of libraries names
-    if(!is.element(i, .packages(all.available=TRUE))){ # check if package is installed
-      install.packages(i, dependencies=TRUE) # install package with dependencies
-    } 
-    library(package=i, character.only=TRUE) # load package
-  } # end for
-}
+# load raw data
+dataRaw <- read.table('data/raw/herringDataRaw.csv', header = TRUE, sep = ',')
 
-# example of use
-# loadInstall(c('stats','base','forecast'))
+# rename column (add human-readable labels)
+dataRawCleaned01 <- rename(dataRaw, 
+    id = X,
+    planktonCfin1 = cfin1, # dostępność planktonu [zagęszczenie Calanus finmarchicus gat. 1]
+    planktonCfin2 = cfin2, # dostępność planktonu [zagęszczenie Calanus finmarchicus gat. 2]
+    planktonChel1 = chel1, # dostępność planktonu [zagęszczenie Calanus helgolandicus gat. 1]
+    planktonChel2 = chel2, # dostępność planktonu [zagęszczenie Calanus helgolandicus gat. 2]
+    planktonLcop1 = lcop1, # dostępność planktonu [zagęszczenie widłonogów gat. 1]
+    planktonLcop2 = lcop2, # dostępność planktonu [zagęszczenie widłonogów gat. 2]
+    fryLeftPrc = fbar, # natężenie połowów w regionie [ułamek pozostawionego narybku]
+    fryAnnual = recr, # roczny narybek [liczba śledzi]
+    fryLeftAnnualPrc = cumf, #łączne roczne natężenie połowów w regionie [ułamek pozostawionego narybku];
+    herringsNumber = totaln, # łączna liczba ryb złowionych w ramach połowu [liczba śledzi]
+    waterTemperature = sst, # temperatura przy powierzchni wody [°C]
+    salinity = sal, # poziom zasolenia wody [Knudsen ppt]
+    monthnr = xmonth, # miesiąc połowu [numer miesiąca]
+    nothAtlanticOscillation = nao # oscylacja północnoatlantycka [mb]
+)
